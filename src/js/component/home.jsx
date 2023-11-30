6ea31b5d3ee4575baca3bc0af9a8b2af1bbf85ba
Jose Dominguez <141463676+jdomi020@users.noreply.github.com>
2023-11-30 05:54:07 +0000

    Tinkering with the code

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 
 //create your first component
@@ -6,6 +6,47 @@ const Home = () => {
 	const [inputValue, setInputValue] = useState("");
 	const [todos, setTodos] = useState([]);
  
+
+
+
+// Create a library of functions for requests and Promises
+
+// function status(response) {
+// 	if (response.status >= 200 && response.status < 300) {
+// 		return Promise.resolve(response)
+// 	} else {
+// 		return Promise.reject(new Error(response.statusText))
+// 	}
+// }
+
+// Create a GET Request for your username
+
+
+
+// Create a POST Request for your username
+
+// fetch("https://playground.4geeks.com/apis/fake/todos/user/jdomi020", {
+// 	method: "POST",
+// 	headers: {
+// 		"Content-type": "application/json; charset=UTF-8"
+// 	},
+// 	body: JSON.stringify({key: "value"})
+// })
+// .then(response => {
+// 	if (!response.ok) {
+// 		throw new Error('Network response was not ok.');
+// 	}
+// 	return response.json(); // Process the response if needed
+// })
+// .then(data => {
+// 	console.log("Request succeeded with JSON response", data);
+// })
+// .catch(error => {
+// 	console.error("Request failed", error);
+// });
+
+// Create a PUT Request
+// Create a DELETE Request
 	//Add into array -> concat
 	//Delete from array -> filter
 	//Update -> map
@@ -19,20 +60,20 @@ const Home = () => {
 						type="text"
 						onChange={(e) => setInputValue(e.target.value)}
 						value={inputValue}
-						onKeyPress={(e) => {
+						onKeyDown={(e) => {
 							if(e.key === "Enter") {
 						 		setTodos(todos.concat([inputValue]));
 								setInputValue("");
 							}
 						}}
-						placeholder="What do you need to do?"></input><i class="fa-solid fa-trash-can"></i>
+						placeholder="What do you need to do?"></input><i className="fa-solid fa-trash-can"></i>
 				</li>
 
 				{todos.map((item, index) => (
 					<li>
 						{item}{""} 
 						<i 
-							class="fa-solid fa-trash-can"
+							className="fa-solid fa-trash-can"
 							onClick ={() =>
 								setTodos(
 									todos.filter(
